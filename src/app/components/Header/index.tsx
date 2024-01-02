eb17e5ca24bc1015faada06246c42c69769e5e9a
João Victor Vasconcelos Flôr <danteopzz@hotmail.com>
2024-01-02 18:32:15 -0300

    first commit

---
+++ src/app/components/Header/index.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image"
+
+export function Header() {
+    return (
+        <div className="flex justify-between container m-auto py-5">
+            <h1>Logo</h1>
+            <h2>Icone de bars</h2>
+        </div>
+    )
+}
